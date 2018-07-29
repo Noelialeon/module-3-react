633b89586a95186d44df5ddf9eeee8ff1bbab8b4
Noelia León <noelials@hotmail.es>
2018-07-29 14:35:34 +0200

    search-bar done

--- labs/lab-react-data-binding/starter-code/src/index.js
+++ labs/lab-react-data-binding/starter-code/src/index.js
@@ -1,18 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import foods from "./foods.json";
+import "bulma/css/bulma.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { foods: foods, actualFood: foods, query: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let searchTerm = event.target.value;
+    let result = [];
+    for (let i = 0; i < foods.length; i++) {
+      if(foods[i].name.toUpperCase().indexOf(searchTerm.toUpperCase()) !== -1){
+        result.push(foods[i])
+      }
+      this.setState({
+        query: event.target.value,
+        actualFood: result
+      });
+    }
+  }
+
   render() {
     return (
       <div>
-        {/* Your application code */}
+      <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+        {this.state.actualFood.map((food, i) => (
+          <div key={i} id={i} className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-64x64">
+                  <img src={food.image} alt="image-not-found" />
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{food.name}</strong> <br />
+                    <small>{food.calories} cal</small>
+                  </p>
+                </div>
+              </div>
+              <div className="media-right">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input className="input" type="number" value="1" />
+                  </div>
+                  <div className="control">
+                    <button className="button is-info">+</button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
